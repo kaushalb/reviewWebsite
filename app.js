56ac15aed47d959aa5884678d66c71e3c308cec7
KaushalB <kaushal.bommena@gmail.com>
2020-12-28 23:49:22 -0800

    API inserted

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 $(document).ready(() => {
     $('#searchBar').on('submit', (e) => {
         console.log($('#searchText').val());
+        getMovie(searchText);
         e.preventDefault();
     })
 });
 
 function getMovie(searchText){
-
+    axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=64d7fa7"+searchText).then((response) => {
+        console.log(response);
+    }).catch((err) => {
+        console.log(err);
+    });
 }
