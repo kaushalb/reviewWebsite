f58aaa19021d023d4b05f0dd1a5d0f8183fcf4c8
KaushalB <kaushal.bommena@gmail.com>
2020-12-29 21:16:14 -0800

    searchbar fix

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 $(document).ready(() => {
     $('#searchBar').on('submit', (e) => {
         console.log($('#searchText').val());
-        getMovie(searchText);
+        getMovie($('#searchText').val());
+        //console.log('http://www.omdbapi.com/?apikey=64d7fa7'+'&t='+searchText);
         e.preventDefault();
     })
 });
 
 function getMovie(searchText){
-    axios.get('http://www.omdbapi.com/?apikey=64d7fa7'+'&t'+searchText).then((response) => {
+    axios.get('http://www.omdbapi.com/?apikey=64d7fa7'+'&s='+searchText).then((response) => {
         console.log(response);
     }).catch((err) => {
         console.log(err);
