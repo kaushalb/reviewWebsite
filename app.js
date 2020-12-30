44c36e71ac8585403cd0ff590f6f216fcbaa3718
KaushalB <kaushal.bommena@gmail.com>
2020-12-29 21:30:24 -0800

    title search bug fix

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ $(document).ready(() => {
     $('#searchBar').on('submit', (e) => {
         console.log($('#searchText').val());
         getMovie($('#searchText').val());
-        //console.log('http://www.omdbapi.com/?apikey=64d7fa7'+'&t='+searchText);
         e.preventDefault();
     })
 });
@@ -10,6 +9,23 @@ $(document).ready(() => {
 function getMovie(searchText){
     axios.get('http://www.omdbapi.com/?apikey=64d7fa7'+'&s='+searchText).then((response) => {
         console.log(response);
+
+        let movies = response.data.Search;
+        let output = '';
+        $.each(movies, (index, movie) => {
+            output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5>${movie.Title}</h5>
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Check Review</a>
+                    </div>
+                </div>       
+            `;
+        })
+
+        $('#movies').html(output);
+
     }).catch((err) => {
         console.log(err);
     });
